d00eb8da92dd64b9ca8182bbf2e135b610b69d4b
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-09-11 17:22:20 +0200

    remove first and last character of string

--- codewars.js
+++ codewars.js
@@ -1,7 +1,11 @@
-//Complete the solution so that it reverses all of the words within the string passed in.
+//It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
-function reverseWords(str){
-  return str.split(" ").reverse().toString().replaceAll(",", " ")
-}
+function removeChar(str){
+  let newString = ""
+  for (let i = 1; i < str.length - 1; i++) {
+    newString += str[i]
+  }
+  return newString
+};
 
-console.log(reverseWords("hi my name is Rina"))
+console.log(removeChar("Rina"))

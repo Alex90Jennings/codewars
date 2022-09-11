d4bf796527f3657c46d0f5027e450ff5f8dae262
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-09-11 17:42:34 +0200

    calculate messi's goals

--- codewars.js
+++ codewars.js
@@ -1,11 +1,5 @@
-//It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+// given messi's goals across three leagues, calculate his total goals
 
-function removeChar(str){
-  let newString = ""
-  for (let i = 1; i < str.length - 1; i++) {
-    newString += str[i]
-  }
-  return newString
-};
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals
 
-console.log(removeChar("Rina"))
+console.log(goals(10,12,18))
